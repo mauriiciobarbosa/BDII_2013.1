260108f0818031a74717f55309e2f4f7536637c4
mauriiciobarbosa <mauriiciobarbosa@gmail.com>
2013-06-04 17:13:28 -0300

    Correcao de bugs nas Functions

--- DML/Functions/uf_despesas_por_produto.sql
+++ DML/Functions/uf_despesas_por_produto.sql
@@ -1,20 +1,41 @@
-CREATE FUNCTION uf_despesas_por_produto
+-- Retornos : Nome do produto, Quantidade adiquirida, preço unitário, valor total (por produto)
+CREATE FUNCTION uf_despesas_por_produto( @num_licitacao CHAR(13) )
+RETURNS @despesas_por_produtoTab TABLE
 		(
-			@num_licitacao CHAR(13)
+			nom_basico 			VARCHAR(50) 	NOT NULL,
+			qtd_item			INT				NOT NULL,
+			val_preco_unitario	DECIMAL(12,2)	NOT NULL,
+			val_total			DECIMAL(12,2)	NOT NULL
 		)
-RETURNS TABLE
-AS
-	RETURN
+BEGIN
+	-- Criar uma tabela temporaria contendo somente as cotações vencedoras para determinada licitação
+	/*
+	CREATE TABLE #cotacoes_vencedoras
 	(
-		SELECT P.nom_basico AS Produto,
-			   SUM(PL.qtd_item) AS Quantidade,
-			   SUM(CL.val_preco_unitario) AS Valor,
-			   SUM(PL.qtd_item) * SUM(CL.val_preco_unitario) AS Total
-		FROM dbo.produtos_licitacao PL
-		INNER JOIN dbo.cotacoes_licitacao CL
-			ON (CL.num_licitacao = PL.num_licitacao AND CL.num_item = PL.num_item)
-		INNER JOIN dbo.produto P
-			ON (P.num_item = PL.num_item)
-		WHERE PL.num_licitacao = @num_licitacao
-		GROUP BY P.nom_basico
-	);
+		num_licitacao 		CHAR(13) 		NOT NULL,
+		num_item 	 		CHAR(14) 		NOT NULL,
+		val_preco_unitario	DECIMAL(12,2) NOT NULL
+	)
+	-- Preenche a tabela temporaria somente com as cotações vencedoras
+	INSERT INTO #cotacoes_vencedoras (num_licitacao, num_item, val_preco_unitario)
+	SELECT 	num_licitacao, num_item, MIN(val_preco_unitario) FROM dbo.cotacoes	
+	WHERE num_licitacao = @num_licitacao
+	GROUP BY num_licitacao, num_item
+	*/
+
+	SELECT 	num_licitacao, num_item, MIN(val_preco_unitario) 
+	INTO #cotacoes_vencedoras	
+	FROM dbo.cotacoes	
+	WHERE num_licitacao = @num_licitacao
+	GROUP BY num_licitacao, num_item	
+
+	INSERT @despesas_por_produtoTab
+	SELECT nom_basico AS PRODUTO, qtd_item AS QUANTIDADE, VAL_PRECO_UNITARIO AS 'PRECO UNITARIO',
+			VAL_PRECO_UNITARIO * QTD_ITEM AS TOTAL
+	FROM dbo.produto p 
+	INNER JOIN dbo.produtos_licitacao pl ON p.num_item = pl.num_item
+	INNER JOIN #cotacoes_vencedoras cl ON pl.num_licitacao = cl.num_licitacao AND pl.num_item = cl.num_item
+	WHERE pl.num_licitacao = @num_licitacao 
+
+	RETURN
+END
--- DML/Functions/uf_qtd_cotacoes_por_estado_fornecedor.sql
+++ DML/Functions/uf_qtd_cotacoes_por_estado_fornecedor.sql
@@ -9,15 +9,11 @@ AS
 	(
 		SELECT UF.nom_uf AS [Fornecedor - UF],
 			   COUNT(*) AS [Quantidade]
-		FROM dbo.grupo G
-		INNER JOIN dbo.familia F
-			ON (F.cod_grupo = G.cod_grupo)
+		FROM dbo.familia F
 		INNER JOIN dbo.produto P
 			ON (P.cod_grupo = F.cod_grupo AND P.num_familia = F.num_familia)
-		INNER JOIN dbo.produtos_licitacao PL
-			ON (PL.num_item = P.num_item)
 		INNER JOIN dbo.cotacoes_licitacao CL
-			ON (CL.num_licitacao = PL.num_licitacao AND CL.num_item = PL.num_item)
+			ON (CL.num_item = P.num_item)
 		INNER JOIN dbo.fornecedor FO
 			ON (FO.num_fornecedor = CL.num_fornecedor)
 		INNER JOIN dbo.uf UF
@@ -25,4 +21,5 @@ AS
 		WHERE G.desc_grupo = @nom_grupo
 		AND F.desc_familia = @nom_familia
 		GROUP BY UF.nom_uf
+		ORDER BY 2 DESC
 	);
