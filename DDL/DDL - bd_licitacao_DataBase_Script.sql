b380700efda24d1b4c84df869631e0ef2642e484
mauriiciobarbosa <mauriiciobarbosa@gmail.com>
2013-06-04 11:56:01 -0300

    Correcao de Bugs no arquivo de criacao do Banco

--- DDL/DDL - bd_licitacao_DataBase_Script.sql
+++ DDL/DDL - bd_licitacao_DataBase_Script.sql
@@ -3,38 +3,38 @@ ON PRIMARY
 (
 	NAME = bd_licitacao,
 	FILENAME = 'D:\Faculdade\5º SEMESTRE\Banco de Dados II\1º Bimestre\Trabalho\DataBase\bd_licitacao.mdf',
-	SIZE = 5,
+	SIZE = 50,
 	MAXSIZE = 200,
-	FILEGROWTH = 5
+	FILEGROWTH = 50
 ),
 FILEGROUP FGDATA
 (
 	NAME = bd_licitacao_1,
-	FILENAME = 'D:\Faculdade\5º SEMESTRE\Banco de Dados II\1º Bimestre\Trabalho\DataBase\bd_licitacao_1.mdf',
-	SIZE = 5,
-	MAXSIZE = 200,
-	FILEGROWTH = 1
+	FILENAME = 'D:\Faculdade\5º SEMESTRE\Banco de Dados II\1º Bimestre\Trabalho\DataBase\bd_licitacao_1.ndf',
+	SIZE = 50,
+	MAXSIZE = 100,
+	FILEGROWTH = 25
 ),
 (
 	NAME = bd_licitacao_2,
-	FILENAME = 'D:\Faculdade\5º SEMESTRE\Banco de Dados II\1º Bimestre\Trabalho\DataBase\bd_licitacao_2.mdf',
-	SIZE = 5,
-	MAXSIZE = 200,
-	FILEGROWTH = 1
+	FILENAME = 'D:\Faculdade\5º SEMESTRE\Banco de Dados II\1º Bimestre\Trabalho\DataBase\bd_licitacao_2.ndf',
+	SIZE = 50,
+	MAXSIZE = 100,
+	FILEGROWTH = 25
 ),
 FILEGROUP FGINDEX
 (
 	NAME = bd_licitacao_3,
-	FILENAME = 'D:\Faculdade\5º SEMESTRE\Banco de Dados II\1º Bimestre\Trabalho\DataBase\bd_licitacao_3.mdf',
-	SIZE = 5,
-	MAXSIZE = 200,
-	FILEGROWTH = 1
+	FILENAME = 'D:\Faculdade\5º SEMESTRE\Banco de Dados II\1º Bimestre\Trabalho\DataBase\bd_licitacao_3.ndf',
+	SIZE = 50,
+	MAXSIZE = 100,
+	FILEGROWTH = 25
 )
 LOG ON
 (
 	NAME = bd_licitacao_log,
-	FILENAME = 'D:\Faculdade\5º SEMESTRE\Banco de Dados II\1º Bimestre\Trabalho\DataBase\bd_licitacao_log.mdf',
-	SIZE = 5,
+	FILENAME = 'D:\Faculdade\5º SEMESTRE\Banco de Dados II\1º Bimestre\Trabalho\DataBase\bd_licitacao_log.ldf',
+	SIZE = 200,
 	MAXSIZE = 2GB,
-	FILEGROWTH = 10%
+	FILEGROWTH = 200
 )
